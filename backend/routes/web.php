db2d974e764844dba8989594d68e016861fc2c34
FirstVDS Aleksandr <noster2@gmail.com>
2026-02-10 14:39:23 +0300

    fix: вывод Vue

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return file_get_contents(public_path('dist/index.html'));
 });
+
+// Все остальные не-API роуты тоже отдаем Vue
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('dist/index.html'));
+})->where('any', '^(?!api|dist).*$');
